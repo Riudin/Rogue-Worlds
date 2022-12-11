2a616e3aa92351a144b283eb8d807c979b077294
Christoph Sengestack <christoph@sengestack.de>
2022-12-11 15:13:11 +0100

    fixed left-right animation issue
    also completely revamped jump system

---
+++ Godot Project/Items/DefaultGun.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+export(int) var cooldown = 10
+export(int) var p_range = 100
+export(int) var speed = 400
+export(int) var damage = 1
+
+export(PackedScene) onready var bullet
+onready var aim_pos = get_node("AimPosition")
+
+var last_shot = 0
+
+
+func _physics_process(_delta):
+	last_shot += 1
+	#look_at(get_global_mouse_position())
+#	if self.global_position.x > aim_pos.global_position.x:
+#		$Sprite.flip_v = true
+#	else:
+#		$Sprite.flip_v = false
+
+func fire():
+	if last_shot < cooldown: return
+	else: last_shot = 0
+	
+	var projectile = bullet.instance()
+	#projectile.add_to_group("projectiles")
+	get_node("/root").add_child(projectile)
+
+	#projectile.add_to_group("Projectile_" + str(get_parent()))
+	
+	var projectile_direction = aim_pos.global_position - self.global_position
+
+	projectile.position = self.global_position
+	projectile.rotation = projectile_direction.angle()
+	projectile.projectile_direction = projectile_direction
+	projectile.projectile_speed = speed
+	projectile.damage = damage
+	projectile.projectile_range = p_range
--- Godot Project/Player/Player.gd
+++ Godot Project/Player/Player.gd
@@ -1,38 +1,45 @@
 extends KinematicBody2D
 
 
-export(int) var JUMP_FORCE = -250
-export(int) var JUMP_RELEASE_FORCE = -70
-export(int) var MAX_SPEED = 100
+export(int) var RUN_SPEED = 150
 export(int) var ACCELERATION = 20
 export(int) var FRICTION = 15
-export(int) var GRAVITY = 4
-export(int) var ADDITIONAL_FALL_GRAVITY = 4
-export(int) var MAX_FALL_SPEED = 200
-export(int) var DASH_SPEED = 500
+export(int) var MAX_FALL_SPEED = 400
+export(int) var DASH_SPEED = 600
+
+
+export(float) var JUMP_HEIGHT = 100.0
+export(float) var JUMP_TIME_TO_PEAK = 0.5
+export(float) var JUMP_TIME_TO_DESCEND = 0.4
+
+onready var jump_velocity : float = (2.0 * JUMP_HEIGHT) / JUMP_TIME_TO_PEAK * -1.0
+onready var jump_gravity : float = (-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_PEAK * JUMP_TIME_TO_PEAK) * -1.0
+onready var fall_gravity : float = (-2.0 * JUMP_HEIGHT) / (JUMP_TIME_TO_DESCEND * JUMP_TIME_TO_DESCEND) * -1.0
+
 
 onready var animationPlayer = $AnimationPlayer
-onready var leftFootSprite = $Sprites/LeftFoot
-onready var rightFootSprite = $Sprites/RightFoot
-onready var backHandSprite = $Sprites/BackHand
-onready var bodySprite = $Sprites/Body
-onready var frontHandSprite = $Sprites/FrontHand
-onready var headSprite = $Sprites/Head
+#onready var leftFootSprite = $Sprites/LeftFoot
+#onready var rightFootSprite = $Sprites/RightFoot
+#onready var backHandSprite = $Sprites/BackHand
+#onready var bodySprite = $Sprites/Body
+#onready var frontHandSprite = $Sprites/FrontHand
+#onready var headSprite = $Sprites/Head
 
 var velocity = Vector2.ZERO
 var dashing = false
 var facing_right = true setget orient_sprites
 
 func _physics_process(delta):
-	apply_gravity()
+	velocity.y += get_gravity() * delta
+	velocity.y =  min(velocity.y, MAX_FALL_SPEED)
 	var input = Vector2.ZERO
 	input.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	
 	if Input.is_action_just_pressed("dash_right"):
-		self.facing_right = true
+		#self.facing_right = true
 		dash(1)
 	elif Input.is_action_just_pressed("dash_left"):
-		self.facing_right = false
+		#self.facing_right = false
 		dash(-1)
 	
 	if input.x == 0:
@@ -42,43 +49,53 @@ func _physics_process(delta):
 	else:
 		apply_acceleration(input.x)
 		if is_on_floor():
-			animationPlayer.play("Walk")
+			animationPlayer.play("Run")
 		
 		if input.x > 0:
-			self.facing_right = true
+			#self.facing_right = true
+			if velocity.x < 0:
+				velocity.x *= 0
 		elif input.x < 0:
-			self.facing_right = false
+			if velocity.x > 0:
+				velocity.x *= 0
+			#self.facing_right = false
 	
 	if Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_FORCE
-			
+			jump()
 	
-	if is_on_floor() == false:
+	if not is_on_floor():
 		animationPlayer.play("Jump")
-		if Input.is_action_just_released("jump") and velocity.y < JUMP_RELEASE_FORCE:
-			velocity.y = JUMP_RELEASE_FORCE
 	
-	if velocity.y > 0:
-		velocity.y += ADDITIONAL_FALL_GRAVITY
+	if dashing:
+		velocity.y = -40
+		velocity.y = move_toward(velocity.y, 0, (get_gravity() * delta) / 2)
+	
+	if self.global_position <= get_global_mouse_position():
+		self.facing_right = true
+	else:
+		self.facing_right = false
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
-
-func apply_gravity():
-	velocity.y += GRAVITY
-	velocity.y =  min(velocity.y, MAX_FALL_SPEED)
 
 func apply_friction():
 	velocity.x = move_toward(velocity.x, 0, FRICTION)
 
 func apply_acceleration(amount):
-	velocity.x = move_toward(velocity.x, MAX_SPEED * amount, ACCELERATION)
+	velocity.x = move_toward(velocity.x, RUN_SPEED * amount, ACCELERATION)
+
+func get_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump():
+	velocity.y = jump_velocity
+	if dashing:
+		dashing = false
+		velocity.x = velocity.x * 0.2
 
 func dash(dir):
 	dashing = true
 	velocity.x = DASH_SPEED * dir
-	velocity.y = -40
 	yield(get_tree().create_timer(0.3), "timeout")
-	velocity.x = move_toward(velocity.x, 0, 0.05)
 	dashing = false
 
 func orient_sprites(right_is_new):
